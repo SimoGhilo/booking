d9a03449099ce515b5e248c99772fa4361b23b69
Simone Ghilotti <simoneghilotti@gmail.com>
2024-09-01 14:28:08 +0100

    working on searchbar

--- frontend/src/components/SearchBar.js
+++ frontend/src/components/SearchBar.js
@@ -1,6 +1,5 @@
 import React, { useState, useRef } from 'react';
-import DatePicker from 'react-datepicker';
-import 'react-datepicker/dist/react-datepicker.css';
+
 
 
 
@@ -11,50 +10,45 @@ function SearchBar() {
   const [startDate, setStartDate] = useState();
   const [endDate, setEndDate] = useState();
   const [open, setOpen] = useState(false);
+  const [guests, setGuests] = useState(1);
   const datePickerRef = useRef(null);
 
+  // Date function for date input, toISOString() returns the date in YYYY-MM-DD, parameter accepted by input
+  let today = new Date();
+  let localeDate = today.toISOString().split('T')[0];
+
+  
 
   return (
   <>
     <form className="form-inline my-2 my-lg-0">
         <div className='searchbar'>
           <input className="form-control mr-sm-2 search-bar-blue" type="search" placeholder="Where are you going ?" aria-label="Search"/>
-          <DatePicker
-              selected={startDate}
-              onChange={(date) => {
-                setStartDate(date);
-                setOpen(false);
-              }}
-              open={open}
-              onClickOutside={() => setOpen(false)}
-              onClickInside={() => setOpen(true)}
-              ref={datePickerRef}
-              dateFormat="MM/dd/yyyy"
-              className="startDateInput"
-              placeholderText="Check in"
-              onFocus={() => setOpen(true)}
-              onBlur={() => setOpen(false)}
-            />
-          <DatePicker
-              selected={endDate}
-              onChange={(date) => {
-                setEndDate(date);
-                setOpen(false);
-              }}
-              open={open}
-              onClickOutside={() => setOpen(false)}
-              onClickInside={() => setOpen(true)}
-              ref={datePickerRef}
-              dateFormat="MM/dd/yyyy"
-              className="endDateInput"
-              placeholderText="Check out"
-              onFocus={() => setOpen(true)}
-              onBlur={() => setOpen(false)}
-            />
+          <input type='date' className='date-input' min={localeDate} onChange={(event) => {
+            event.preventDefault();
+            setStartDate(event.currentTarget.value);
+          }}/>
+          <input type='date' className='date-input' onChange={(event) => {
+            event.preventDefault();
+            setEndDate(event.currentTarget.value);
+          }}/>
           <div class="guests-box">
               <p>Guests ?</p>
-              <button className='btn btn-primary'>+</button>
-              <button className='btn btn-primary'>-</button>
+              <button className='btn btn-primary plusminus' onClick={(event) => {
+                event.preventDefault();
+                 if(guests <= 3)
+                  {
+                    setGuests(guests + 1);
+                  }
+              }}>+</button>
+              <p className='guestsP'>{guests}</p>
+              <button className='btn btn-primary plusminus' onClick={(event) => {
+                event.preventDefault();
+                 if(guests > 1)
+                  {
+                    setGuests(guests - 1);
+                  }
+              }}>-</button>
           </div>
           <button className="btn btn-outline-light my-2 my-sm-0" type="submit">Search</button>
         </div>
