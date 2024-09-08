288ad3870737b67c4c29083b31e84f92dea4f44e
Simone Ghilotti <simoneghilotti@gmail.com>
2024-09-08 14:33:36 +0100

    search landing page done

--- frontend/src/components/Explore.js
+++ frontend/src/components/Explore.js
@@ -6,6 +6,9 @@ import img2 from "../img/976997.jpg";
 import img3 from "../img/977262.jpg";
 import img4 from "../img/140012002.jpg";
 import img5 from "../img/140012293.jpg";
+import card1 from '../img/card1.jpg';
+import card2 from '../img/card2.jpg';
+import card3 from '../img/card3.jpg';
 
 // Array of image sources
 const srcs = [img1, img2, img3, img4, img5];
@@ -18,7 +21,7 @@ function Explore() {
   return (
     <div className='grid-container'>
       <div className="box">
-        <h2>Explore the UK</h2>
+        <h2 className='title'>Explore the UK</h2>
 
         <Carousel
           autoPlay
@@ -38,6 +41,21 @@ function Explore() {
             </div>
           ))}
         </Carousel>
+      </div>
+      <h2 className='title'>Get inspiration for your next trip...</h2>
+      <div className='flex-box-row'>
+          <div className='card-v'>
+            <img src={card3} className='img-card-v' />
+            <h4 className='overlay-text'>Santorini</h4>
+          </div>
+          <div className='card-h'>
+            <img src={card2} className='img-card-h' />
+            <h4 className='big-text'>Cappadocia</h4>
+          </div>
+          <div className='card-h'>
+            <img src={card1} className='img-card-h' />
+            <h4 className='big-text'>London</h4>
+          </div>
       </div>
     </div>
   );
